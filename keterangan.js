121daaf4817e9c4104024bc6324d24e3206da126
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-13 12:45:22 +0800

    Add files via upload

---
+++ keterangan.js
@@ -0,0 +1,19 @@
+const bgbahaya = document.getElementsByClassName("bgbahaya");
+for (let i = 0; i < bgbahaya.length; i++) {
+  bgbahaya[i].innerHTML = "<h1 class='keterangan-font-size'>Situs ini <span class='color-red'>berbahaya</span> menurut kami berdasarkan laporan dari admin kami yang telah mencoba di situs ini, situs ini dapat menyebabkan <span class='color-red'>ketidak nyamanan untuk pemain</span> berikut penyebab ketidak nyamanan  ?<br/>1. Tidak membayar kemenangan besar pemain<br/>2. withdraw tidak di proses<br/>3. password id pemain di ubah tanpa persetujuan pemilik id<br/>4. Hak-hak member tidak di berikan<br/>5. Mempersulit pemain dengan berbagai alasan<br/>6. id anda dapat di masuki oleh admin tanpa persetujuan anda terlebih dahulu dan saldo di mainkan sampai habis, biasanya jika ini terjadi pada id anda ciri-cirinya anda tidak akan bisa login dengan password yang terakhir anda gunakan sebab sudah di ubah admin untuk bisa masuk ke dalam id anda<br/><br/>Semua situs yang kami tandai <span class='color-red'>merah</span> pernah melakukan salah satu hal di atas sebab itu kami beri tanda warna <span class='color-red'>MERAH</span> yang berarti <span class='color-red'>BERBAHAYA</span><br/>Kami rekomendasikan situs yang di beri warna <span class='color-green'>HIJAU</span> yang artimya <span class='color-green'>AMAN</span> belum pernah ada keanehan selama kami coba<br/></h1>";
+}
+
+const bgwaspada = document.getElementsByClassName("bgwaspada");
+for (let i = 0; i < bgwaspada.length; i++) {
+  bgwaspada[i].innerHTML = "<h1 class='keterangan-font-size'>Situs ini belum di coba oleh admin kami apabila anda ingin mencoba bermain di situs ini anda <span class='color-red'>perlu waspada</span> karena bisa saja situs ini tidak aman, kami merekomendasikan situs yang kami beri tanda <span class='color-green'>warna hijau yang tandanya aman</span> dan sudah di uji coba admin kami</h1>";
+}
+
+const bgdomain = document.getElementsByClassName("bgdomain");
+for (let i = 0; i < bgdomain.length; i++) {
+  bgdomain[i].innerHTML = "<h1 class='keterangan-font-size'>Tidak semua situs kami update hanya situs yang benar-benar aman menurut kami yang akan di update setiap hari, jangan lupa tekan tombol UPDATE terlebih dahulu agar seluruh link terbaru dapat digunakan untuk tombol UPDATE ada di sudut kiri atas<br/>Situs berstatus <span class='color-green'>AMAN BERWARNA HIJAU</span> Link terbaru update otomatis 12 jam 1 kali<br/>Situs berstatus <span class='color-yellow'>WASPADA BERWARNA KUNING</span> link terbaru update otomatis 1 bulan 1 kali di tanggal 1<br/>Situs berstatus <span class='color-red'>BAHAYA BERWARNA MERAH</span> link terbaru update otomatis 1 bulan 1 kali di tanggal 1</h1>";
+}
+
+const bgchatadmin = document.getElementsByClassName("bgchatadmin");
+for (let i = 0; i < bgchatadmin.length; i++) {
+  bgchatadmin[i].innerHTML = "<h1 class='keterangan-font-size'>Tombol CHAT ini di sediakan untuk berkomunikasi dengan admin</br></br>Jika ingin mengirmkan pesan untuk admin tanpa perlu jawaban gunakan chat <span class='color-green'>TAWK</span></br>Contoh 1 </br>tambahkan situs Lotto01 ke dalam</br>Contoh 2</br>min tambahin situs Kenzo188</br>Contoh 3</br>min link Lotto01 tidak bisa buka tolong update</br>intinya sampaikan seluruh hal yang ingin anda sampaikan dalam chat <span class='color-green'>TAWK</span> ini, nanti akan di baca oleh admin dan di kerjakan tanpa membalas pesan anda</br></br>Jika ingin mengirmkan pesan yang akan di balas oleh admin maka gunakan <span class='color-blue'>FACEBOOK</span> untuk menghubungi admin</h1>";
+}
