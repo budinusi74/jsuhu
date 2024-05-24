b70ac5b0d24692f5466a24dddc5796f780909806
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-24 19:20:32 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -161,5 +161,5 @@ function xchatfb() {window.open("https://www.facebook.com/profile.php?id=6155349
 function xbetcoinasia() {window.open("https://btcoinasia.pro/?ref=bunsin");}
 function xjangkaslot() {window.open("https://noblockss.blogspot.com/");}
 function xtogeltoto() {window.open("https://noblockss.blogspot.com/");}
-function xpphoki() {window.open("https://pphoki77.shop/?ref=rakuyaba");}
+function xpphoki() {window.open("https://pph0ki.live/?ref=rakuyaba");}
 function xupdate() {window.open("http://judisuhu.online/");}
