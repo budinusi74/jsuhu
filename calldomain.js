6db84cc33e641d99cfeb9415f4a5039e13b3e127
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-25 05:25:36 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -70,7 +70,7 @@ function xmpo666() {window.open("https://mpo666at.com/register/WM41P433");}
 function xmpo555() {window.open("https://belhamel-catering.com/register/WR3N6NQ9");}
 function xkadobet() {window.open("https://kadobetaa.online/?ref=rakuyaba");}
 function xsweet777() {window.open("https://sweet77.skin/register?ref=GS2AEAB0SVJ");}
-function xhokiwin() {window.open("https://hokiwinae.com/register/Y5NQP87K");}
+function xhokiwin() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=13036704");}
 function xkas138() {window.open("https://kas138vip.com/?ref=rakuyaba");}
 function xjudi388() {window.open("https://jdapa.top/register?reff=rakuyaba");}
 function xkudetabet98() {window.open("https://tawk.to/chat/6277a9737b967b11798e4e3c/1g2hmrr04");}
