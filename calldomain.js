2a952d7db3bc6d7f9c056c9bd5d706e6d1f9e516
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-26 21:27:01 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -1,4 +1,5 @@
 function xno() {window.open("https://nosafetyy.blogspot.com/");}
+function xarobet88() {window.open("https://arobet88.com/?ref=rakuyaba");}
 function xhedon77() {window.open("https://hedon77c.club/daftar?ref_id=5f6e2101");}
 function xsinarmas77() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=17793120");}
 function xeqn77() {window.open("https://eqn777.xyz/register/EVCWMKG6");}
