99edb27f2b3e25cad90a36c9508e882c5688db87
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-06-02 19:28:15 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -126,7 +126,7 @@ function xhoki138() {window.open("https://secure.livechatinc.com/customer/action
 function xmansion77() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=13085046");}
 function xtatabet() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=4743931");}
 function xsatutogel() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=4743931");}
-function xlotto01() {window.open("https://kemenangan01.me/referral/BUNSIN");}
+function xlotto01() {window.open("https://seleramimpi.xyz/referral/BUNSIN");}
 function xdewatogel() {window.open("https://secure.livechatinc.com/licence/5501791/v2/open_chat.cgi");}
 function xsquad777() {window.open("https://pro.squad777-play.site/register?ref=bunsin");}
 function xmpo888() {window.open("https://secure.livechatenterprise.com/licence/14751111/v2/open_chat.cgi?groups=0");}
