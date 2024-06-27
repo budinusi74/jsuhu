a25da57cc414a808bc2300e0d9ef9c04ff42899f
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-06-28 07:39:46 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -47,7 +47,7 @@ function xrajatoto3() {location.replace("https://rajatoto3blade.com/register?ref
 function xmpo666() {location.replace("https://mpo666gs.com/register/WM41P433");}
 function xmpo555() {location.replace("https://mpo555-cover.xyz/register/WR3N6NQ9");}
 function xkadobet() {location.replace("https://kadobetindo.net/?ref=rakuyaba");}
-function xsweet777() {location.replace("https://sweet77.wiki/register?ref=GS2AEAB0SVJ");}
+function xsweet777() {location.replace("https://sweet77.today/register?ref=GS2AEAB0SVJ");}
 function xkas138() {location.replace("https://kas138vip.com/?ref=rakuyaba");}
 function xjudi388() {location.replace("https://jdmerah.top/register?reff=rakuyaba");}
 function xmega55() {location.replace("https://maison-kayser-usa.com/register?ref=ZGGIGAB00JK");}
